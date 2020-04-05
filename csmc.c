e197ad81ae6e6fb3accdc1cd83012787f0cc06be
Dheeraj Yalamanchili <dheeraj@Dheerajs-MacBook-Pro.local>
2020-04-05 15:56:40 -0500

    added structs for threads

--- csmc.c
+++ csmc.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <pthread.h>
-#define NUM_THREADS 5
 
 void *StudentThread(void *data)
 {
@@ -12,6 +12,7 @@ void *StudentThread(void *data)
 
 void *TutorThread(void *data)
 {
+	printf("Student Thread Running!\n");
 	pthread_exit(NULL);
 }
 
@@ -20,10 +21,29 @@ void *CoordinatorThread(void *data)
 	pthread_exit(NULL);
 }
 
+struct student {
+	pthread_t thread;
+	int id;
+	int visits;
+};
+
+struct tutor {
+	pthread_t thread;
+	int id;
+	bool idle;
+	int currentStudent;
+};
+
+struct chair {
+	int id;
+	bool taken;
+	int currentStudent;
+};
+
 int main(int argc, char *argv[])
 {
-	int NUM_STUDENTS, NUM_TUTORS; NUM_CHAIRS, NUM_HELP;
-	
+	// Read arguments for number of threads / help
+	int NUM_STUDENTS, NUM_TUTORS, NUM_CHAIRS, NUM_HELP;
 	if(argc ==  5){
 		NUM_STUDENTS = atoi(argv[1]);
 		NUM_TUTORS = atoi(argv[2]);
@@ -35,12 +55,26 @@ int main(int argc, char *argv[])
 		exit(-1);
 	}
 
-	pthread_t threads[NUM_THREADS];
+	// Define datastructures to hold info about threads / chairs
+	struct student *students = (struct student*) malloc(NUM_STUDENTS * sizeof(struct student));
+	struct tutor *tutors = (struct tutor*) malloc(NUM_TUTORS * sizeof(struct tutor));
+	struct tutor *chairs = (struct chair*) malloc(NUM_CHAIRS * sizeof(struct chair));
+
 	int rc; long t;
-	for(t = 0; t < NUM_THREADS; t++){
+	for(t = 0; t < NUM_STUDENTS; t++){
 		// DEBUG PRINT STATEMENT
 		printf("MAIN: Creating Student Thread #%ld\n", t);
-		rc = pthread_create(&threads[t], NULL, StudentThread, (void *)t);
+		rc = pthread_create(&students[t].thread, NULL, StudentThread, (void *)t);
+		if(rc) {
+			//DEBUG PRINT STATEMENT
+			printf("MAIN ERROR: Error Creating Thread! Code: %d\n", rc);
+			exit(-1);
+		}
+	}
+	for(t = 0; t < NUM_TUTORS; t++){
+		// DEBUG PRINT STATEMENT
+		printf("MAIN: Creating Tutor Thread #%ld\n", t);
+		rc = pthread_create(&tutors[t].thread, NULL, StudentThread, (void *)t);
 		if(rc) {
 			//DEBUG PRINT STATEMENT
 			printf("MAIN ERROR: Error Creating Thread! Code: %d\n", rc);
